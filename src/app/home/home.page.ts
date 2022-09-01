173b86cd2dab26eaf239765147eca245f6be0995
Amin <medamineharwel@outlook.com>
2022-09-01 18:36:10 +0100

    quick test google signin on android in the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,9 +9,27 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  signedId = false;
 
   constructor() {
     GoogleAuth.initialize();
+  }
+
+  async signIn() {
+    const user = await GoogleAuth.signIn();
+    this.signedId = true;
+    console.log(user);
+  }
+
+  async signOut() {
+    const resp = await GoogleAuth.signOut();
+    this.signedId = false;
+    console.log(resp);
+  }
+
+  async refresh() {
+    const authObj = await GoogleAuth.refresh();
+    console.log(authObj);
   }
 
 }
